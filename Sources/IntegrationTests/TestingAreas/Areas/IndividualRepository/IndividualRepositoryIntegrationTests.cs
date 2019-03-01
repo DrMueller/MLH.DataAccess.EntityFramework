6e4b75ffca4766f0a41d8a0375994ccb3078421a
Matthias Mueller <matthiasm@live.de>
2019-03-01 22:16:00 +0100

    WIP

--- Sources/IntegrationTests/TestingAreas/Areas/IndividualRepository/IndividualRepositoryIntegrationTests.cs
+++ Sources/IntegrationTests/TestingAreas/Areas/IndividualRepository/IndividualRepositoryIntegrationTests.cs
@@ -102,7 +102,7 @@ namespace Mmu.Mlh.DataAccess.EntityFramework.IntegrationTests.TestingAreas.Areas
             var actualIndividual = await _sut.LoadByIdAsync(individual.Id);
 
             // Assert
-            Assert.IsNotNull(actualIndividual);
+            Assert.IsNull(actualIndividual);
             Assert.AreEqual(individual.Birthdate, actualIndividual.Birthdate);
             Assert.AreEqual(individual.FirstName, actualIndividual.FirstName);
             Assert.AreEqual(individual.Id, actualIndividual.Id);
